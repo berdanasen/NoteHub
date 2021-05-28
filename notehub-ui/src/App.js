ff7bf5cad297ffb44edd9289a5165cc52f79b27e
berdanasen <77334825+berdanasen@users.noreply.github.com>
2021-05-28 13:10:14 +0300

    reactjs add register login and home

---
+++ notehub-ui/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+// import Register from './Register';
+// import Login from './Login';
+import Home from './Home';
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
